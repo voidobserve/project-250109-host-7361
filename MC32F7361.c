67691d2f9b4d0c0692b1eb72648b3a81df849d65
voidobserve <99012263+voidobserve@users.noreply.github.com>
2025-01-10 18:58:58 +0800

    2025-01-10，暂存，基本功能完成

---
+++ MC32F7361.c
@@ -0,0 +1,523 @@
+/******************************************************************************
+;  *       @型号                 : MC32F7361
+;  *       @创建日期             : 2021.12.21
+;  *       @公司/作者            : SINOMCU-FAE
+;  *       @晟矽微技术支持       : 2048615934
+;  *       @晟矽微官网           : http://www.sinomcu.com/
+;  *       @版权                 : 2021 SINOMCU公司版权所有.
+;  *----------------------摘要描述---------------------------------
+;  *
+******************************************************************************/
+
+#include "user.h"
+
+/************************************************
+;  *    @函数名          : CLR_RAM
+;  *    @说明            : 清RAM
+;  *    @输入参数        :
+;  *    @返回参数        :
+;  ***********************************************/
+void CLR_RAM(void)
+{
+    for (FSR0 = 0; FSR0 < 0xff; FSR0++)
+    {
+        INDF0 = 0x00;
+    }
+    FSR0 = 0xFF;
+    INDF0 = 0x00;
+}
+/************************************************
+;  *    @函数名            : IO_Init
+;  *    @说明              : IO初始化
+;  *    @输入参数          :
+;  *    @返回参数          :
+;  ***********************************************/
+void IO_Init(void)
+{
+    IOP0 = 0x00;   // io口数据位
+    OEP0 = 0x3F;   // io口方向 1:out  0:in
+    PUP0 = 0x00;   // io口上拉电阻   1:enable  0:disable
+    PDP0 = 0x00;   // io口下拉电阻   1:enable  0:disable
+    P0ADCR = 0x00; // io类型选择  1:模拟输入  0:通用io
+
+    // IOP1 = 0x00;   // io口数据位
+    IOP1 = 0xD0;   // 不点亮LED
+    OEP1 = 0xFF;   // io口方向 1:out  0:in
+    PUP1 = 0x00;   // io口上拉电阻   1:enable  0:disable
+    PDP1 = 0x00;   // io口下拉电阻   1:enable  0:disable
+    P1ADCR = 0x00; // io类型选择  1:模拟输入  0:通用io
+
+    IOP2 = 0x00; // io口数据位
+    OEP2 = 0x0F; // io口方向 1:out  0:in
+    PUP2 = 0x00; // io口上拉电阻   1:enable  0:disable
+    PDP2 = 0x00; // io口下拉电阻   1:enable  0:disablea
+
+    PMOD = 0x00;  // P00、P01、P13 io端口值从寄存器读，推挽输出
+    DRVCR = 0xB0; // P16、P17输出驱动电流100mA
+}
+
+/************************************************
+;  *    @函数名            : ADC_Init
+;  *    @说明              : ADC初始化
+;  *    @输入参数          :
+;  *    @返回参数          :
+;  ***********************************************/
+void adc_config(void)
+{
+    P15OE = 0; // 输入模式 -- 变成高阻态
+    // P15DC = 1; // 模拟模式
+
+    ADCR0 = 0xAB; // 12位精度，使能adc,adc通道选择  1/4 VDD
+    ADCR1 = 0x80; // adc转换时钟选择 FHIRC/32，使用內部2.0V参考电压
+    ADCR2 = 0x0F; // 采样时间，只能固定是15 个 ADCLK
+}
+
+// 获取adc单次转换后的值
+u16 adc_get_val(void)
+{
+    u8 i = 0; // adc采集次数的计数
+    u16 g_temp_value = 0;
+    u32 g_tmpbuff = 0;
+    u16 g_adcmax = 0;
+    u16 g_adcmin = 0xFFFF;
+
+    // 采集20次，去掉前两次采样，再去掉一个最大值和一个最小值，再取平均值
+    for (i = 0; i < 20; i++)
+    {
+        ADEOC = 0; // 清除ADC转换完成标志位，启动AD转换
+        while (!ADEOC)
+            ;                // 等待转换完成
+        g_temp_value = ADRH; // 取出转换后的值
+        g_temp_value = g_temp_value << 4 | (ADRL & 0x0F);
+        if (i < 2)
+            continue; // 丢弃前两次采样的
+        if (g_temp_value > g_adcmax)
+            g_adcmax = g_temp_value; // 最大
+        if (g_temp_value < g_adcmin)
+            g_adcmin = g_temp_value; // 最小
+        g_tmpbuff += g_temp_value;
+    }
+    g_tmpbuff -= g_adcmax;           // 去掉一个最大
+    g_tmpbuff -= g_adcmin;           // 去掉一个最小
+    g_temp_value = (g_tmpbuff >> 4); // 除以16，取平均值
+
+    return g_temp_value;
+}
+
+// 获取adc单次转换后的值
+u16 adc_get_val_once(void)
+{
+    u16 g_temp_value = 0;
+    ADEOC = 0; // 清除ADC转换完成标志位，启动AD转换
+    while (!ADEOC)
+        ;                // 等待转换完成
+    g_temp_value = ADRH; // 取出转换后的值
+    g_temp_value = g_temp_value << 4 | (ADRL & 0x0F);
+    return g_temp_value;
+}
+
+// 定时器3
+void timer3_config(void)
+{
+    T3LOAD = 135 - 1; // FCPU 64分频后，这里是1ms触发一次中断（用计算出来的值会有误差，这里加上了一些补偿）
+    T3CR = 0x86;      // 使能定时器，时钟源选择FCPU，64分频
+    T3IE = 1;
+}
+
+/************************************************
+;  *    @函数名            : Sys_Init
+;  *    @说明              : 系统初始化
+;  *    @输入参数          :
+;  *    @返回参数          :
+;  ***********************************************/
+void Sys_Init(void)
+{
+    GIE = 0;
+    CLR_RAM();
+    IO_Init();
+
+    LED_RED_OFF();
+    LED_GREEN_OFF();
+    LED_BLUE_OFF();
+
+    // 按键检测引脚
+    P00PU = 1; // 上拉
+    P00OE = 0; // 输入模式
+
+// 充电检测引脚:
+#if USE_MY_DEBUG
+    P05OE = 0; // 输入
+#else
+    P04OE = 0; // 输入模式
+#endif
+
+    adc_config();
+    timer3_config();
+
+    GIE = 1;
+}
+
+// 按键扫描函数，最后会得到对应的按键事件
+// 需要放在10ms的定时器中
+void key_scan(void)
+{
+    static volatile u8 last_key_id = KEY_ID_NONE;
+    static volatile u8 press_cnt = 0;               // 按键按下的时间计数
+    static volatile u8 filter_cnt = 0;              // 按键消抖，使用的变量
+    static volatile u8 filter_key_id = KEY_ID_NONE; // 按键消抖时使用的变量
+
+    volatile u8 cur_key_id = KEY_ID_NONE;
+    static volatile u8 flag_is_key_mode_hold = 0;
+
+    if (0 == KEY_SCAN_PIN)
+    {
+        cur_key_id = KEY_ID_BUTTOM;
+    }
+    else
+    {
+        cur_key_id = KEY_ID_NONE;
+    }
+
+    if (cur_key_id != filter_key_id)
+    {
+        // 如果有按键按下/松开
+        filter_cnt = 0;
+        filter_key_id = cur_key_id;
+        return;
+    }
+
+    if (filter_cnt < KEY_FILTER_TIMES)
+    {
+        // 如果检测到相同的按键按下/松开
+        // 防止计数溢出
+        filter_cnt++;
+        return;
+    }
+
+    // 滤波/消抖完成后，执行到这里
+
+    if (last_key_id != cur_key_id)
+    {
+        if (last_key_id == KEY_ID_NONE)
+        {
+            // 如果有按键按下，清除按键按下的时间计数
+            press_cnt = 0;
+        }
+        else if (cur_key_id == KEY_ID_NONE)
+        {
+            // 如果按键松开
+            if (press_cnt < 75)
+            {
+                // 按下时间小于 750ms ，是短按
+                if (KEY_ID_BUTTOM == last_key_id)
+                {
+                    //  按键短按
+                    key_event = KEY_EVENT_PRESS;
+                }
+            }
+            else
+            {
+                // 长按、长按持续之后松手
+                flag_is_key_mode_hold = 0;
+            }
+        }
+    }
+    else if (cur_key_id != KEY_ID_NONE)
+    {
+        // 如果按键按住不放
+        if (press_cnt < 255)
+            press_cnt++;
+
+        if (0 == flag_is_key_mode_hold)
+        {
+            if (press_cnt >= 100) // xx ms
+            {
+                key_event = KEY_EVENT_HOLD;
+                flag_is_key_mode_hold = 1;
+            }
+        }
+    }
+
+    last_key_id = cur_key_id;
+}
+
+// 对扫描到的按键事件进行处理
+void key_event_handle(void)
+{
+    if (KEY_EVENT_PRESS == key_event)
+    {
+        if (flag_is_dev_open)
+        {
+            if (0 == led_mode)
+            {
+                // 紫光 - > 红
+                LED_BLUE_OFF();
+                LED_RED_ON();
+                led_mode = 1;
+            }
+            else if (1 == led_mode)
+            {
+                // 红->蓝
+                LED_RED_OFF();
+                LED_BLUE_ON();
+                led_mode = 2;
+            }
+            else if (2 == led_mode)
+            {
+                // 其他情况 - > 紫光 （红 + 蓝）
+                LED_RED_ON();
+                LED_BLUE_ON();
+                led_mode = 0;
+            }
+
+            power_off_cnt = 0; // 清空关机计数
+        }
+    }
+    else if (KEY_EVENT_HOLD == key_event)
+    {
+        if (flag_is_dev_open)
+        {
+            LED_RED_OFF();
+            LED_BLUE_OFF();
+            flag_is_dev_open = 0;
+        }
+        else
+        {
+            // 关机->开机
+            LED_RED_ON();
+            LED_BLUE_ON();
+            led_mode = 0; // 表示当前是紫光
+            flag_is_dev_open = 1;
+        }
+    }
+
+    // 处理完成后，清除按键事件
+    key_event = KEY_EVENT_NONE;
+}
+
+/************************************************
+;  *    @函数名            : main
+;  *    @说明              : 主程序
+;  *    @输入参数          :
+;  *    @返回参数          :
+;  ***********************************************/
+void main(void)
+{
+    Sys_Init();
+    while (1)
+    {
+#if 1
+        // 检测是否在充电
+        if (0 == flag_is_in_charging)
+        {
+            if (CHARGE_SCAN_PIN)
+            {
+                delay_ms(20);
+                if (CHARGE_SCAN_PIN)
+                {
+                    flag_is_in_charging = 1;
+                }
+            }
+        }
+        else
+        {
+            // 充电时，检测是否断开了充电
+            if (0 == CHARGE_SCAN_PIN)
+            {
+                delay_ms(20);
+                if (0 == CHARGE_SCAN_PIN)
+                {
+                    flag_is_in_charging = 0;
+                    flag_is_full_charged = 0;
+                }
+            }
+        }
+
+        // 检测是否充满电
+        if (flag_is_in_charging)
+        {
+            adc_val = adc_get_val();
+            if (adc_val >= 2145)
+            {
+                flag_is_full_charged = 1;
+            }
+            else
+            {
+                flag_is_full_charged = 0;
+            }
+        }
+
+        if (flag_is_dev_open && 0 == flag_is_in_charging)
+        {
+            adc_val = adc_get_val();
+            if (adc_val < 1638) // 电池电压小于3.2V
+            {
+                flag_is_power_low = 1;
+            }
+
+            if (adc_val < 1485) // 电池电压小于2.9V
+            {
+                flag_is_dev_open = 0; // 关机
+            }
+        }
+        else
+        {
+            flag_is_power_low = 0;
+        }
+
+        if (flag_is_in_charging && flag_is_full_charged)
+        {
+            LED_GREEN_ON();
+        }
+
+        key_event_handle();
+
+        if (0 == flag_is_dev_open &&    //
+            0 == flag_is_in_charging && // 充电时，不进入低功耗
+            KEY_SCAN_PIN)               // 有按键按下，不进入低功耗
+        {
+            // 进入低功耗
+            LED_BLUE_OFF();
+            LED_GREEN_OFF();
+            LED_RED_OFF();
+
+            GIE = 0; // 禁用所有中断
+            // LED引脚配置为输入:
+            P17OE = 0;
+            P13OE = 0;
+            P16OE = 0;
+
+            T3EN = 0;
+            T3IE = 0;
+
+            // 按键配置为键盘中断触发 
+            P00KE = 1;
+// 充电检测引脚配置为键盘中断触发
+#if USE_MY_DEBUG
+            P05KE = 1;
+#else
+            P04KE = 1;
+#endif
+            KBIF = 0;
+            KBIE = 1;
+
+            HFEN = 0; // 关闭高速时钟
+            LFEN = 1;
+
+            // 休眠前关闭外设
+            Nop();
+            Nop();
+            Stop();
+            Nop();
+            Nop();
+
+            HFEN = 1; // 开启高速时钟
+            P00KE = 0; 
+#if USE_MY_DEBUG
+            P05KE = 0;
+#else
+            P04KE = 0;
+#endif
+            KBIE = 0;
+            KBIF = 0;
+
+            // T3EN = 1;
+            // T3IE = 1;
+            // // LED驱动引脚配置回输出模式:
+            // P17OE = 1;
+            // P13OE = 1;
+            // P16OE = 1;
+            Sys_Init(); // 按键检测内部的标志位和静态变量没有清除，这里直接清除所有变量的值，全为0,并且重新初始化
+            delay_ms(1);
+            GIE = 1;
+        }
+
+#endif
+
+        __asm;
+        clrwdt;
+        __endasm;
+    }
+}
+/************************************************
+;  *    @函数名            : interrupt
+;  *    @说明              : 中断函数
+;  *    @输入参数          :
+;  *    @返回参数          :
+;  ***********************************************/
+void int_isr(void) __interrupt
+{
+    __asm;
+    movra _abuf;
+    swapar _PFLAG;
+    movra _statusbuf;
+    __endasm;
+
+    if (T3IF & T3IE)
+    {
+        // 目前每1ms进入一次中断
+        { // 按键扫描
+            static u8 key_scan_cnt = 0;
+            key_scan_cnt++;
+            if (key_scan_cnt >= 10)
+            {
+                key_scan_cnt = 0;
+                key_scan();
+            }
+        } // 按键扫描
+
+        { // 充电、低电量时，负责灯光闪烁
+            static u16 blink_cnt = 0;
+            if (flag_is_power_low ||
+                (flag_is_in_charging && 0 == flag_is_full_charged))
+            {
+                blink_cnt++;
+                if (blink_cnt <= 500)
+                {
+                    LED_GREEN_ON();
+                }
+                else if (blink_cnt <= 1000)
+                {
+                    LED_GREEN_OFF();
+                }
+                else
+                {
+                    blink_cnt = 0;
+                    LED_GREEN_OFF();
+                }
+            }
+            else
+            {
+                blink_cnt = 0;
+                LED_GREEN_OFF();
+            }
+        }
+
+        { // 开机后，负责自动关机
+            if (flag_is_dev_open)
+            {
+                power_off_cnt++;
+                if (power_off_cnt >= 360000) // 6min
+                {
+                    power_off_cnt = 0;
+                    flag_is_dev_open = 0;
+                }
+            }
+            else
+            {
+                power_off_cnt = 0;
+            }
+        }
+
+        T3IF = 0;
+    }
+
+    __asm;
+    swapar _statusbuf;
+    movra _PFLAG;
+    swapr _abuf;
+    swapar _abuf;
+    __endasm;
+}
+
+/**************************** end of file *********************************************/
---
+++ user.h
@@ -0,0 +1,222 @@
+/******************************************************************************
+;  *       @型号                   : MC32F7361
+;  *       @创建日期               : 2021.12.21
+;  *       @公司/作者              : SINOMCU-FAE
+;  *       @晟矽微技术支持         : 2048615934
+;  *       @晟矽微官网             : http://www.sinomcu.com/
+;  *       @版权                   : 2021 SINOMCU公司版权所有.
+;  *---------------------- 建议 ---------------------------------
+;  *                   变量定义时使用全局变量
+******************************************************************************/
+#ifndef USER
+#define USER
+#include "mc32-common.h"
+#include "MC32F7361.h"
+
+/*****************************************************************
+;       Function : Define variables
+;*****************************************************************/
+
+#define u8 unsigned char
+#define u16 unsigned int
+#define u32 unsigned long int
+#define uint8_t unsigned char
+#define uint16_t unsigned int
+#define uint32_t unsigned long int
+
+#define DEF_SET_BIT0 0x01
+#define DEF_SET_BIT1 0x02
+#define DEF_SET_BIT2 0x04
+#define DEF_SET_BIT3 0x08
+#define DEF_SET_BIT4 0x10
+#define DEF_SET_BIT5 0x20
+#define DEF_SET_BIT6 0x40
+#define DEF_SET_BIT7 0x80
+
+#define DEF_CLR_BIT0 0xFE
+#define DEF_CLR_BIT1 0xFD
+#define DEF_CLR_BIT2 0xFB
+#define DEF_CLR_BIT3 0xF7
+#define DEF_CLR_BIT4 0xEF
+#define DEF_CLR_BIT5 0xDF
+#define DEF_CLR_BIT6 0xBF
+#define DEF_CLR_BIT7 0x7F
+
+#define FAIL 1
+#define PASS 0
+
+#define USE_MY_DEBUG 0
+
+#define LED_BLUE_PIN P17D
+#define LED_GREEN_PIN P13D
+#define LED_RED_PIN P16D
+#define KEY_SCAN_PIN P00D
+#if USE_MY_DEBUG
+#define CHARGE_SCAN_PIN P05D
+#else
+#define CHARGE_SCAN_PIN P04D
+#endif
+
+#define LED_BLUE_ON()         \
+	{                         \
+		do                    \
+		{                     \
+			LED_BLUE_PIN = 0; \
+		} while (0);          \
+	}
+#define LED_BLUE_OFF()        \
+	{                         \
+		do                    \
+		{                     \
+			LED_BLUE_PIN = 1; \
+		} while (0);          \
+	}
+#define LED_GREEN_ON()         \
+	{                          \
+		do                     \
+		{                      \
+			LED_GREEN_PIN = 1; \
+		} while (0);           \
+	}
+#define LED_GREEN_OFF()        \
+	{                          \
+		do                     \
+		{                      \
+			LED_GREEN_PIN = 0; \
+		} while (0);           \
+	}
+#define LED_RED_ON()         \
+	{                        \
+		do                   \
+		{                    \
+			LED_RED_PIN = 0; \
+		} while (0);         \
+	}
+#define LED_RED_OFF()        \
+	{                        \
+		do                   \
+		{                    \
+			LED_RED_PIN = 1; \
+		} while (0);         \
+	}
+
+enum
+{
+	KEY_ID_NONE,
+	KEY_ID_BUTTOM,
+};
+enum
+{
+	KEY_EVENT_NONE,
+	KEY_EVENT_PRESS,
+	KEY_EVENT_HOLD,
+};
+
+#define KEY_FILTER_TIMES 2
+volatile u8 key_event;
+
+u32 power_off_cnt; // 负责自动关机的时间计数
+
+//===============Field Protection Variables===============
+volatile u8 abuf;
+volatile u8 statusbuf;
+volatile u16 adc_val;
+
+//===============Global Variable===============
+u8 i; // 循环计数值
+
+volatile u8 led_mode;
+
+//============Define  Flag=================
+typedef union
+{
+	unsigned char byte;
+	struct
+	{
+		u8 bit0 : 1;
+		u8 bit1 : 1;
+		u8 bit2 : 1;
+		u8 bit3 : 1;
+		u8 bit4 : 1;
+		u8 bit5 : 1;
+		u8 bit6 : 1;
+		u8 bit7 : 1;
+	} bits;
+} bit_flag;
+volatile bit_flag flag1;
+#define flag_is_dev_open flag1.bits.bit0
+#define flag_is_in_charging flag1.bits.bit1
+#define flag_is_full_charged flag1.bits.bit2
+#define flag_is_power_low flag1.bits.bit3
+
+// 毫秒级延时 (误差：在1%以内，1ms、10ms、100ms延时的误差均小于1%)
+// 前提条件：FCPU = FHOSC / 4
+void delay_ms(u16 xms)
+{
+	while (xms)
+	{
+		u16 i = 572;
+		while (i--)
+		{
+			Nop();
+		}
+		xms--; // 把 --操作放在while()判断条件外面，更节省空间
+
+		__asm;
+		clrwdt; // 喂狗
+		__endasm;
+	}
+}
+
+// #if USE_MY_DEBUG
+#define DEBUG_PIN P22D
+#if 0  // 以下程序约占用81字节空间
+// 通过一个引脚输出数据(发送一次约400ms)
+// #define DEBUG_PIN P22D
+void send_data_msb(u32 send_data)
+{
+	// 先发送格式头
+	// __set_input_pull_up(); // 高电平
+	DEBUG_PIN = 1;
+	delay_ms(15);
+	// __set_output_open_drain(); // 低电平
+	DEBUG_PIN = 0;
+	delay_ms(7); //
+
+	for (u8 i = 0; i < 32; i++)
+	{
+		if ((send_data >> (32 - 1 - i)) & 0x01)
+		{
+			// 如果要发送逻辑1
+			// __set_input_pull_up();  	   // 高电平
+			DEBUG_PIN = 1;
+			delay_ms(5); //
+			// __set_output_open_drain(); // 低电平
+			DEBUG_PIN = 0;
+			delay_ms(10); //
+		}
+		else
+		{
+			// 如果要发送逻辑0
+			// __set_input_pull_up();  	   // 高电平
+			DEBUG_PIN = 1;
+			delay_ms(5); //
+			// __set_output_open_drain(); // 低电平
+			DEBUG_PIN = 0;
+			delay_ms(5); //
+		}
+	}
+
+	// 最后，设置为低电平
+	// __set_output_open_drain(); // 低电平
+	DEBUG_PIN = 0;
+	delay_ms(1);
+	DEBUG_PIN = 1;
+	delay_ms(1);
+	DEBUG_PIN = 0;
+}
+#endif // #if USE_MY_DEBUG
+
+#endif
+
+/**************************** end of file *********************************************/
